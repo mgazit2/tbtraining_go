34b8bd8a53932f61596a6667e96159bc4223a5f6
mgazit2 <mgazit2@icloud.com>
2022-02-02 17:15:31 -0600

    checkpoint -> moving port to variable

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module GO_REST
 
 go 1.16
+
+require github.com/scraly/learning-go-by-examples/go-rest-api v0.0.0-20210825073107-25d8173e695c // indirect
--- internal/main.go
+++ internal/main.go
@@ -5,13 +5,40 @@ import(
 	"html"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
     println("Hello, world!")
+	var count int = 0
+	var port string = "3001"
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		_, err := fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		if err != nil {
+			return 
+		}
 	})
-	log.Println("Listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) {
+		count++
+		_, err := fmt.Fprintf(w, "You have been here %d times", count)
+		if err != nil {
+			return 
+		}
+	})
+	//define paths
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "About page")
+		if err != nil {
+			return 
+		}
+	})
+	//define path /time that displays current datetime
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "The time is %v", time.Now())
+		if err != nil {
+			return 
+		}
+	})
+	log.Printf("Listening on localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
