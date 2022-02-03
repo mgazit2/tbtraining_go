5a4ffb07787109215afed214992a17bfeb3725f5
mgazit2 <mgazit2@icloud.com>
2022-02-03 16:48:59 -0600

    even/odd assignment 1 can be ran by inputing 1 when prompted by program for an int

---
+++ assignments/one/main.go
@@ -0,0 +1,43 @@
+package one
+
+import(
+"fmt"
+"html"
+"log"
+"net/http"
+"time"
+)
+
+func main() {
+	println("Hello, world!")
+	var count int = 0
+	var port string = "3001"
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		if err != nil {
+			return
+		}
+	})
+	http.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) {
+		count++
+		_, err := fmt.Fprintf(w, "You have been here %d times", count)
+		if err != nil {
+			return
+		}
+	})
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "About page")
+		if err != nil {
+			return
+		}
+	})
+	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "The time is %v", time.Now())
+		if err != nil {
+			return
+		}
+	})
+	log.Printf("Listening on localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
